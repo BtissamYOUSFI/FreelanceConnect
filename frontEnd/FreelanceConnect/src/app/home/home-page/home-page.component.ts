4ee18d00f72b234fe267b61fb7eaadb2ae8383e7
YOUSFI Btissam <ibtissamyousfi111@gmail.com>
2024-10-31 12:48:44 +0100

    hAWDGVIU

--- frontEnd/FreelanceConnect/src/app/home/home-page/home-page.component.ts
+++ frontEnd/FreelanceConnect/src/app/home/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BodyComponent } from '../body/body.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [BodyComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
--- frontEnd/FreelanceConnect/tailwind.config.js
+++ frontEnd/FreelanceConnect/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '8xl': '1100px',
+      }
+    },
   },
   plugins: [],
 }
